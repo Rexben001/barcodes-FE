8a6283154134dd3cd320e6604a4e04f9a8f38af9
Benjamin <benjamin@eulercoder.com>
2020-08-16 19:14:42 +0100

    add loader

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,14 @@
 <template>
   <form>
+    <loading
+      :active.sync="isLoading"
+      :can-cancel="true"
+      :on-cancel="onCancel"
+      :is-full-page="fullPage"
+    ></loading>
+
     <p>Login to get all your qrcodes</p>
-    <p class="error">{{error}}</p>
+    <p class="error">{{ error }}</p>
     <input placeholder="Email address" v-model="email" type="email" />
     <input placeholder="Password" v-model="password" type="password" />
     <button id="submit" @click.prevent="submitted">Login</button>
@@ -10,32 +17,39 @@
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios';
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
 
 export default {
-  name: "Login",
-  data: function () {
+  name: 'Login',
+  data: function() {
     return {
-      email: "",
-      password: "",
-      error: "",
+      email: '',
+      password: '',
+      error: '',
+      isLoading: false,
     };
+  },
+  components: {
+    Loading,
   },
   methods: {
     async submitted() {
+      this.isLoading = true;
       try {
         const result = await axios.post(
-          "https://2dz7gb09o9.execute-api.us-east-1.amazonaws.com/dev/get",
+          'https://2dz7gb09o9.execute-api.us-east-1.amazonaws.com/dev/get',
           {
             password: this.password,
             email: this.email,
           }
         );
-        localStorage.setItem("barcodeToken", result.data.token);
-        this.$router.push("/dashboard");
-        window.location.href = "/dashboard";
+        localStorage.setItem('barcodeToken', result.data.token);
+        window.location.href = '/dashboard';
       } catch (error) {
         this.error = error.response.data.message;
+        this.isLoading = false;
       }
     },
   },
