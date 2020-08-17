566497294510a6b06a621706c4dae41701ff6406
Rexben <rexben.rb@gmail.com>
2020-08-17 22:46:05 +0100

    add routing

--- src/components/Scan.vue
+++ src/components/Scan.vue
@@ -1,93 +1,104 @@
 <template>
   <div class="scan-page">
     <div class="tab">
-      <p>Camera Scan</p>
-      <p>Drag and Drop Scan</p>
-      <p>Upload Scan</p>
+      <p
+        @click.prevent="clicked('stream')"
+        v-bind:style="{background: stream ? '#9EB369': '', color: stream ? '#fff': 'inherit'}"
+      >Camera Scan</p>
+      <p
+        @click.prevent="clicked('drop')"
+        v-bind:style="{background: drop ? '#9EB369': '', color: drop ? '#fff': 'inherit'}"
+      >Drag and Drop Scan</p>
+      <p
+        @click.prevent="clicked('capture')"
+        v-bind:style="{background: capture ? '#9EB369': '', color: capture ? '#fff': 'inherit'}"
+      >Upload Scan</p>
     </div>
     <div class="qrcode">
-      <h2>Scan Barcodes</h2>
-      <qrcode-stream
-        v-if="!error"
-        :camera="camera"
-        @decode="onDecode"
-        @init="onInit"
-        class="qrcode-main"
-      />
-      <p v-if="result">Result👇</p>
-      <p v-if="result">{{ result }}</p>
-      <p v-if="error" class="error">{{ error }}</p>
-
-      <h2>Drag and Drop zone</h2>
-      <qrcode-drop-zone
-        @detect="onDetect"
-        @dragover="onDragOver"
-        @init="logErrors"
-      >
-        <div class="drop-area" :class="{ dragover: dragover }">
-          DROP SOME IMAGES HERE
+      <div class="create">
+        <h2 v-if="drag">Drag and Drop zone</h2>
+        <h2 v-if="stream">Scan Barcodes</h2>
+        <h2 v-if="capture">Upload zone</h2>
+        <img src="../assets/qr.svg" />
+      </div>
+      <div v-if="stream">
+        <div id="create">
+          <qrcode-stream
+            v-if="!error"
+            :camera="camera"
+            @decode="onDecode"
+            @init="onInit"
+            class="qrcode-main form"
+          />
+          <div>
+            <p>Result</p>
+            <p v-if="result && !isUrl">{{ result }}</p>
+            <a v-if="result && isUrl" target="_blank" :href="result">{{ result }}</a>
+            <p v-if="error" class="error">{{ error }}</p>
+          </div>
         </div>
-      </qrcode-drop-zone>
-      <p v-if="dragResult">Result👇</p>
-      <p v-if="dragResult">{{ dragResult }}</p>
-      <p v-if="dragError" class="error">{{ dragError }}</p>
+      </div>
 
-      <h2>Upload zone</h2>
-      <qrcode-capture @decode="onDecodeUpload" />
+      <div v-if="drop">
+        <div id="create">
+          <qrcode-drop-zone
+            @detect="onDetect"
+            @dragover="onDragOver"
+            @init="logErrors"
+            class="qrcode-main form"
+          >
+            <div class="qrcode-main form" :class="{ dragover: dragover }">DROP SOME IMAGES HERE</div>
+          </qrcode-drop-zone>
 
-      <p v-if="uploadResult">Result👇</p>
-      <p v-if="uploadResult">{{ uploadResult }}</p>
-      <p v-if="uploadError" class="error">{{ uploadError }}</p>
+          <p>Result</p>
+          <p v-if="dragResult">{{ dragResult }}</p>
+          <p v-if="dragError" class="error">{{ dragError }}</p>
+        </div>
+      </div>
+
+      <div v-if="capture">
+        <div id="create">
+          <qrcode-capture @decode="onDecodeUpload" class="qrcode-main form" />
+          <p v-if="uploadResult">Result</p>
+          <p v-if="uploadResult">{{ uploadResult }}</p>
+          <p v-if="uploadError" class="error">{{ uploadError }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader';
+import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from "vue-qrcode-reader";
 
 export default {
-  name: 'Scan',
+  name: "Scan",
   components: {
     QrcodeStream,
     QrcodeDropZone,
     QrcodeCapture,
   },
-  data: function() {
+  data: function () {
     return {
-      text: '',
-      url: '',
-      result: '',
-      error: '',
-      camera: 'auto',
-      dragResult: '',
-      uploadResult: '',
-      dragError: '',
+      text: "",
+      url: "",
+      result: "",
+      error: "",
+      camera: "auto",
+      dragResult: "",
+      uploadResult: "",
+      dragError: "",
+      stream: true,
+      drop: false,
+      capture: false,
+      isUrl: false,
     };
   },
   methods: {
-    async submitted() {
-      const token = localStorage.getItem('barcodeToken');
-      let headers = {};
-      if (token) {
-        headers = {
-          Authorization: token,
-        };
-      }
-      const result = await axios.post(
-        'https://2dz7gb09o9.execute-api.us-east-1.amazonaws.com/dev/barcodes',
-        {
-          text: this.text,
-        },
-        { headers }
-      );
-
-      this.url = result.data.url;
-    },
-
     async onDecode(result) {
       if (result) {
         this.result = result;
+        this.checkResult(this.result);
         this.turnCameraOff();
         await this.timeout(4000);
         this.turnCameraOn();
@@ -98,28 +109,28 @@ export default {
       try {
         await promise;
       } catch (error) {
-        if (error.name === 'NotAllowedError') {
+        if (error.name === "NotAllowedError") {
           this.error =
-            'You need to grant camera access permisson to scan barcodes';
-        } else if (error.name === 'NotFoundError') {
-          this.error = 'No camera on this device';
-        } else if (error.name === 'NotSupportedError') {
-          this.error = 'Secure context required (HTTPS, localhost)';
-        } else if (error.name === 'NotReadableError') {
-          this.error = 'Is the camera already in use?';
-        } else if (error.name === 'OverconstrainedError') {
-          this.error = 'Installed cameras are not suitable';
-        } else if (error.name === 'StreamApiNotSupportedError') {
-          this.error = 'Stream API is not supported in this browser';
+            "You need to grant camera access permisson to scan barcodes";
+        } else if (error.name === "NotFoundError") {
+          this.error = "No camera on this device";
+        } else if (error.name === "NotSupportedError") {
+          this.error = "Secure context required (HTTPS, localhost)";
+        } else if (error.name === "NotReadableError") {
+          this.error = "Is the camera already in use?";
+        } else if (error.name === "OverconstrainedError") {
+          this.error = "Installed cameras are not suitable";
+        } else if (error.name === "StreamApiNotSupportedError") {
+          this.error = "Stream API is not supported in this browser";
         }
       }
     },
     turnCameraOn() {
-      this.camera = 'auto';
+      this.camera = "auto";
     },
 
     turnCameraOff() {
-      this.camera = 'off';
+      this.camera = "off";
     },
     timeout(ms) {
       return new Promise((resolve) => {
@@ -140,17 +151,29 @@ export default {
         this.dragResult = content;
         this.dragError = null;
       } catch (error) {
-        if (error.name === 'DropImageFetchError') {
+        if (error.name === "DropImageFetchError") {
           this.dragError = "Sorry, you can't load cross-origin images :/";
-        } else if (error.name === 'DropImageDecodeError') {
+        } else if (error.name === "DropImageDecodeError") {
           this.dragError = "Ok, that's not an image. That can't be decoded.";
         } else {
-          this.dragError = 'Ups, what kind of error is this?! ' + error.message;
+          this.dragError = "Ups, what kind of error is this?! " + error.message;
         }
       }
     },
     onDecodeUpload(result) {
       this.uploadResult = result;
+    },
+    clicked(value) {
+      console.log("value", value);
+      this.stream = value === "stream";
+      this.drop = value === "drop";
+      this.capture = value === "capture";
+    },
+    checkResult(result) {
+      const re = /[-a-zA-Z0-9@:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_.~#?&//=]*)?/gi;
+      if (re.test(result)) {
+        this.isUrl = true;
+      }
     },
   },
 };
@@ -171,9 +194,9 @@ img {
   height: 40%;
 }
 .qrcode-main {
-  width: 500px;
+  width: 50%;
   height: auto;
-  margin: 0 auto;
+  /* margin: 0 auto; */
 }
 
 .error {
@@ -184,7 +207,7 @@ img {
   font-size: 1.2rem;
 }
 .qrcode {
-  width: 70%;
+  width: 100%;
 }
 .drop-area {
   width: 500px;
@@ -205,25 +228,47 @@ img {
 .scan-page {
   display: flex;
   box-sizing: border-box;
+  top: 9%;
+  position: absolute;
+  width: 100%;
 }
 
 .tab {
-  width: 30%;
-  padding-left: 2rem;
+  width: 20%;
   box-sizing: border-box;
-  margin-top: -10px;
+  margin-top: 10px;
 }
 .tab > p {
-  font-size: 1.5rem;
+  font-size: 1.3rem;
   cursor: pointer;
-  /* margin-bottom: 1rem; */
   padding: 2rem 0.5rem 2rem 2rem;
   text-align: left;
-  border-bottom: 2px solid #ddd;
+  border-bottom: 0.5px solid #ddd;
+  border-right: 0.5px solid #ddd;
 }
 
 .tab > p:hover {
-  background: lightblue;
-  color: white;
+  background: rgb(165 187 108);
+  color: white !important;
+}
+
+.qrcode {
+  margin-top: 1rem;
+  position: relative;
+}
+
+.qrcode #create {
+  width: 100%;
+  display: flex;
+}
+
+.qrcode #create .form {
+  width: 50%;
+}
+.qrcode .create {
+  height: 35%;
+}
+.qrcode .create img {
+  top: -28px;
 }
 </style>
