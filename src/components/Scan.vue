28980e6bc83da9c09d78cf82255f09d4eb04426d
Rexben <rexben.rb@gmail.com>
2020-08-18 22:37:23 +0100

    add menu bar

--- src/components/Scan.vue
+++ src/components/Scan.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="scan-page">
-    <div class="tab">
+    <p v-if="open" @click.prevent="menuOpen" class="sideMenu">☰</p>
+    <p v-if="!open" @click.prevent="menuOpen" class="sideMenuX">X</p>
+    <div class="tab" :class="open ? '' : 'open'">
       <p
         @click.prevent="clicked('stream')"
-        v-bind:style="{background: stream ? '#9EB369': '', color: stream ? '#fff': 'inherit'}"
-      >Camera Scan</p>
+        v-bind:style="{
+          background: stream ? '#9EB369' : '',
+          color: stream ? '#fff' : 'inherit',
+        }"
+      >
+        Camera Scan
+      </p>
       <p
         @click.prevent="clicked('drop')"
-        v-bind:style="{background: drop ? '#9EB369': '', color: drop ? '#fff': 'inherit'}"
-      >Drag and Drop Scan</p>
+        v-bind:style="{
+          background: drop ? '#9EB369' : '',
+          color: drop ? '#fff' : 'inherit',
+        }"
+      >
+        Drag and Drop Scan
+      </p>
       <p
         @click.prevent="clicked('capture')"
-        v-bind:style="{background: capture ? '#9EB369': '', color: capture ? '#fff': 'inherit'}"
-      >Upload Scan</p>
+        v-bind:style="{
+          background: capture ? '#9EB369' : '',
+          color: capture ? '#fff' : 'inherit',
+        }"
+      >
+        Upload Scan
+      </p>
     </div>
     <div class="qrcode">
       <div class="create">
@@ -23,18 +40,22 @@
       </div>
       <div v-if="stream">
         <div id="create">
-          <qrcode-stream
-            v-if="!error"
-            :camera="camera"
-            @decode="onDecode"
-            @init="onInit"
-            class="qrcode-main form"
-          />
+          <div class="camera">
+            <p v-if="error" class="error">{{ error }}</p>
+            <qrcode-stream
+              v-if="!error"
+              :camera="camera"
+              @decode="onDecode"
+              @init="onInit"
+              class="qrcode-main form"
+            />
+          </div>
           <div>
             <p>Result</p>
             <p v-if="result && !isUrl">{{ result }}</p>
-            <a v-if="result && isUrl" target="_blank" :href="result">{{ result }}</a>
-            <p v-if="error" class="error">{{ error }}</p>
+            <a v-if="result && isUrl" target="_blank" :href="result">{{
+              result
+            }}</a>
           </div>
         </div>
       </div>
@@ -63,7 +84,11 @@
       <div v-if="capture">
         <div id="create">
           <div class="qrcode-main form">
-            <qrcode-capture @decode="onDecodeUpload" class="input" @change="loadFile($event)" />
+            <qrcode-capture
+              @decode="onDecodeUpload"
+              class="input"
+              @change="loadFile($event)"
+            />
           </div>
           <div>
             <p>Result</p>
@@ -77,30 +102,31 @@
 </template>
 
 <script>
-import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from "vue-qrcode-reader";
+import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader';
 
 export default {
-  name: "Scan",
+  name: 'Scan',
   components: {
     QrcodeStream,
     QrcodeDropZone,
     QrcodeCapture,
   },
-  data: function () {
+  data: function() {
     return {
-      text: "",
-      url: "",
-      result: "",
-      error: "",
-      camera: "auto",
-      dragResult: "",
-      uploadResult: "",
-      dragError: "",
-      uploadError: "",
+      text: '',
+      url: '',
+      result: '',
+      error: '',
+      camera: 'auto',
+      dragResult: '',
+      uploadResult: '',
+      dragError: '',
+      uploadError: '',
       stream: true,
       drop: false,
       capture: false,
       isUrl: false,
+      open: true,
     };
   },
   methods: {
@@ -118,28 +144,28 @@ export default {
       try {
         await promise;
       } catch (error) {
-        if (error.name === "NotAllowedError") {
+        if (error.name === 'NotAllowedError') {
           this.error =
-            "You need to grant camera access permisson to scan barcodes";
-        } else if (error.name === "NotFoundError") {
-          this.error = "No camera on this device";
-        } else if (error.name === "NotSupportedError") {
-          this.error = "Secure context required (HTTPS, localhost)";
-        } else if (error.name === "NotReadableError") {
-          this.error = "Is the camera already in use?";
-        } else if (error.name === "OverconstrainedError") {
-          this.error = "Installed cameras are not suitable";
-        } else if (error.name === "StreamApiNotSupportedError") {
-          this.error = "Stream API is not supported in this browser";
+            'You need to grant camera access permisson to scan barcodes';
+        } else if (error.name === 'NotFoundError') {
+          this.error = 'No camera on this device';
+        } else if (error.name === 'NotSupportedError') {
+          this.error = 'Secure context required (HTTPS, localhost)';
+        } else if (error.name === 'NotReadableError') {
+          this.error = 'Is the camera already in use?';
+        } else if (error.name === 'OverconstrainedError') {
+          this.error = 'Installed cameras are not suitable';
+        } else if (error.name === 'StreamApiNotSupportedError') {
+          this.error = 'Stream API is not supported in this browser';
         }
       }
     },
     turnCameraOn() {
-      this.camera = "auto";
+      this.camera = 'auto';
     },
 
     turnCameraOff() {
-      this.camera = "off";
+      this.camera = 'off';
     },
     timeout(ms) {
       return new Promise((resolve) => {
@@ -156,15 +182,15 @@ export default {
       try {
         const { content } = await promise;
         if (!content) {
-          this.dragError = "Unable to scan qrcode, pls provide a valid qrcode";
+          this.dragError = 'Unable to scan qrcode, pls provide a valid qrcode';
         } else this.dragResult = content;
       } catch (error) {
-        if (error.name === "DropImageFetchError") {
+        if (error.name === 'DropImageFetchError') {
           this.dragError = "Sorry, you can't load cross-origin images :/";
-        } else if (error.name === "DropImageDecodeError") {
+        } else if (error.name === 'DropImageDecodeError') {
           this.dragError = "Ok, that's not an image. That can't be decoded.";
         } else {
-          this.dragError = "Ups, what kind of error is this?! " + error.message;
+          this.dragError = 'Ups, what kind of error is this?! ' + error.message;
         }
       }
     },
@@ -172,18 +198,19 @@ export default {
       try {
         if (!result)
           return (this.uploadError =
-            "Unable to scan qrcode, pls provide a valid qrcode");
+            'Unable to scan qrcode, pls provide a valid qrcode');
 
         this.uploadResult = result;
       } catch (error) {
-        console.log("error>>", error);
+        console.log('error>>', error);
       }
     },
     clicked(value) {
-      console.log("value", value);
-      this.stream = value === "stream";
-      this.drop = value === "drop";
-      this.capture = value === "capture";
+      console.log('value', value);
+      this.stream = value === 'stream';
+      this.drop = value === 'drop';
+      this.capture = value === 'capture';
+      this.open = true;
     },
     checkResult(result) {
       const re = /[-a-zA-Z0-9@:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_.~#?&//=]*)?/gi;
@@ -193,13 +220,16 @@ export default {
     },
     loadFile(event) {
       var reader = new FileReader();
-      reader.onload = function () {
+      reader.onload = function() {
         // var output = document.getElementById("output");
         // output.src = reader.result;
 
         console.log(reader.result);
       };
       reader.readAsDataURL(event.target.files[0]);
+    },
+    menuOpen() {
+      this.open = !this.open;
     },
   },
 };
@@ -263,6 +293,7 @@ img {
   width: 20%;
   box-sizing: border-box;
   margin-top: 10px;
+  position: absolute;
 }
 .tab > p {
   font-size: 1.3rem;
@@ -281,6 +312,7 @@ img {
 .qrcode {
   margin-top: 1rem;
   position: relative;
+  /* margin-left: 2rem; */
 }
 
 .qrcode #create {
@@ -298,7 +330,7 @@ img {
   top: -28px;
 }
 
-input[type="file"] {
+input[type='file'] {
   padding: 1rem;
   font-size: 1.5rem;
   background: #6e7d47;
@@ -309,5 +341,45 @@ input[type="file"] {
   width: 100%;
   margin-top: 3rem;
   font-family: cursive;
+}
+
+.sideMenu {
+  font-size: 2.5rem;
+  color: rgb(165 187 108);
+  display: none;
+  margin-top: -5px;
+  position: fixed;
+  z-index: 250;
+  box-shadow: 5px 5px 10px 5px #ddd;
+  border-radius: 7px;
+  cursor: pointer;
+}
+.sideMenuX {
+  font-size: 2.1rem;
+  display: none;
+  margin-top: 10px;
+  position: fixed;
+  z-index: 300;
+  right: 10px;
+  color: #293902;
+  font-weight: 700;
+  cursor: pointer;
+}
+@media only screen and (max-width: 700px) {
+  .sideMenu,
+  .sideMenuX {
+    display: block;
+  }
+  .tab {
+    display: none;
+  }
+  .open {
+    width: 100%;
+    z-index: 200;
+    background: white;
+    height: 80vh;
+    top: -20px;
+    display: block;
+  }
 }
 </style>
