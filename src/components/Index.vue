1566d5dcc495cdee1e91e2f32a91be24d6441f20
Rexben <rexben.rb@gmail.com>
2020-11-26 22:26:57 +0100

    update index

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="main">
-    <h2>Main Page</h2>
-    <img src="../assets/qr.svg" />
-
-    <div>
-      <p>Create QR codes</p>
+    <h2></h2>
+    <div class="first article">
+      <div>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s
+        </p>
+      </div>
+      <img src="../assets/qr.svg" />
+    </div>
+    <div class="first second-article">
+      <img src="../assets/qrboy.svg" />
+      <div>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -20,9 +35,45 @@ export default {
   display: flex;
   justify-content: center;
   width: 100%;
-  position: relative;
+  flex-direction: column;
 }
 h2 {
-  margin-top: 70px;
+  margin-top: 30px;
+}
+.first {
+  display: flex;
+  width: 100%;
+  position: relative;
+}
+.first > div {
+  width: 60%;
+  position: absolute;
+}
+
+.article > div {
+  left: 100px;
+  top: 140px;
+}
+.second-article > div {
+  right: 30px;
+  top: 400px;
+}
+
+.first div p {
+  font-size: 1.5rem !important;
+  font-family: cursive;
+  width: 80%;
+}
+
+.first img {
+  height: 300px;
+  width: auto;
+  position: absolute;
+  right: 200px;
+}
+
+.second-article img {
+  top: 300px;
+  left: 100px;
 }
 </style>
